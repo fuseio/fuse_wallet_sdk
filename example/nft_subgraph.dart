faf6a4bb21c70bd4bf9b3dabc93d26ed194280a7
Lior Agnin <lior.agnin@gmail.com>
2023-04-23 21:47:00 +0300

    Added examples

---
+++ example/nft_subgraph.dart
@@ -0,0 +1,12 @@
+import 'package:fuse_wallet_sdk/fuse_wallet_sdk.dart';
+
+void main() async {
+  // Create a project: https://developers.fuse.io
+  final String publicApiKey = '';
+  final FuseWalletSDK fuseWalletSDK = FuseWalletSDK(publicApiKey);
+  final String owner = '0x...';
+  final res = await fuseWalletSDK.nftModule.getCollectiblesByOwner(
+    owner,
+  );
+  print(res.data);
+}
