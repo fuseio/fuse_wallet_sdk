f5f0f2f6663e1ea0ceff84fc0d0d22ffd6f3b42c
Samet Sahin <a.sahin.ual@gmail.com>
2023-10-26 16:05:21 +0300

    set the DEFAULT_GAS_LIMIT to 700000

--- lib/src/constants/variables.dart
+++ lib/src/constants/variables.dart
@@ -3,7 +3,7 @@ class Variables {
 
   static const FUSE_CHAIN_ID = 122;
   static const FUSE_RPC_URL = 'https://rpc.fuse.io';
-  static const int DEFAULT_GAS_LIMIT = 1000000;
+  static const int DEFAULT_GAS_LIMIT = 700000;
   static const String NATIVE_TOKEN_ADDRESS =
       '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'; // For sending native (ETH/FUSE) tokens
   static const BASE_URL = 'api.fuse.io';
